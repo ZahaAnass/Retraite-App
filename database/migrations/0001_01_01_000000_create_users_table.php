1dc2d205a63a0fb737014aec66ea6b30ecf9f78f
ZahaAnass <zahaanass277@gmail.com>
2026-01-04 13:22:19 +0100

    update user migration

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -15,8 +15,10 @@ return new class extends Migration
             $table->id();
             $table->string('name');
             $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            // 'admin', 'retraite', 'demandeur'
+            $table->string('role')->default('demandeur');
+            $table->text('bio')->nullable(); // For retiree profile
             $table->rememberToken();
             $table->timestamps();
         });
